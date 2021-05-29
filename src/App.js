5963c63ff0d12bb211b7ac2d931327decc95b150
unknown <trex33160@gmail.com>
2021-05-29 22:21:50 +0545

    made the site and added logic

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import "./App.css";
 import { useState } from "react";
 function App() {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState("");
+  const [inputValue, setInputValue] = useState("");
   const [result, setResult] = useState(0);
 
   const Submit = (e) => {
     e.preventDefault();
-    setResult(value);
-    translateToString(fibonacci(result));
-    setValue(0);
+    setResult(translateToString(fibonacci(value)));
+    setInputValue(value);
+    setValue("");
   };
 
   const fibonacci = (n) => {
     // we will be using array in array instead of number
     let array = [[0], [1]];
-    for (let i = 2; i <= value; i++) {
-      array.push(add(array[i - 2], array[i - 1]));
+    for (let i = 2; i <= n; i++) {
+      array.push(add(array[i - 1], array[i - 2]));
     }
+    return array[n];
   };
 
   // function that takes an array and length as an arguemnt. Then it converts the array into the same length and pushes undefined to 0 beacause undefined cannot be used in mathematical calculation
@@ -31,38 +33,47 @@ function App() {
   //adds two arrays
   const add = (arr1, arr2) => {
     // checks if array length is equal if not calls the convert function
-    // if arr1 = [1,2,4,5] and arr2 = [1, 5, 1, 3, 5] it will conver arr1 to [1,2,4,5,0]
-    if (arr1 > arr2) {
+    // if arr1 = [1,2,4,5] and arr2 = [1, 5, 1, 3, 5] it will convert arr1 to [1,2,4,5,0]
+    if (arr1.length > arr2.length) {
       arr2 = convert(arr2, arr1.length);
-    } else if (arr1 < arr2) {
+    } else if (arr1.length < arr2.length) {
       arr1 = convert(arr1, arr2.length);
     }
-    // sum is the number we
+    // sum is the array we push to when we add each element from arr1 and arr2
     let sum = [];
     let transfer = 0;
     for (let i = 0; i < arr1.length; i++) {
+      // if the sum[i] is single digit then we just push it directly to sum
+
       if (arr1[i] + arr2[i] + transfer < 10) {
         sum[i] = arr1[i] + arr2[i] + transfer;
+        transfer = 0;
       } else {
+        //else we push the remainder for eg (9+8+0)%10 = 7 and set transfer to 1
         sum[i] = (arr1[i] + arr2[i] + transfer) % 10;
         transfer = 1;
       }
     }
+    //if transfer is 1 we push it to the sum
     if (transfer) {
       sum.push(transfer);
     }
     return sum;
   };
-  const translateToString = (arr) => {
-    let string = "";
-    for (let i = arr.length - 1; i >= 0; i++) {
-      string = string + arr[i];
+
+  //convert the array to string
+  //[1,2,4,5,6,7] =====> '765421'
+  const translateToString = (array) => {
+    var str = "";
+    for (let i = array.length - 1; i >= 0; i--) {
+      str = str + array[i];
     }
-    return string;
+    return str;
   };
   return (
     <div className="App">
-      <form onSubmit={Submit}>
+      <h1>Fibonacci</h1>
+      <form className="form" onSubmit={Submit}>
         <input
           type="text"
           value={value}
@@ -70,12 +81,15 @@ function App() {
             setValue(e.target.value);
           }}
         />
-        <button>Get Fib</button>
+        <button className="button">Get Fib</button>
       </form>
+
       {result !== 0 ? (
-        <p>
-          The {value} number in the Fibonacci sequentce is {result}
-        </p>
+        <div className="result">
+          <p>The {inputValue} number in fibonacci sequence is :</p>
+
+          <p>{result}</p>
+        </div>
       ) : (
         ""
       )}
